d1c8b84aa1ea8d0482f97952502617329613639b
copeve1015 <copeve1015@gmail.com>
2024-05-23 16:49:59 +0900

    create new project

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import createBootstrap from 'bootstrap-vue-next'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(createBootstrap({ components: true, directives: true }))
+app.use(router)
+
+app.mount('#app')
